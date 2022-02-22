de6332a5bddbe8d40ded5a68350b75f5f526be4c
unic0rn9k <unic0rn9k@protonmail.com>
2022-02-22 05:04:23 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+///! # Numbers
+///!
+///! # Thanks
+///! Heavy inspiration has been taken from crates listed bellow.
+///!
+///! - https://lib.rs/crates/num
+///! - https://lib.rs/crates/fast-floats
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
