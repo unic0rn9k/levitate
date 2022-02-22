b85cf3eea608d7349804f750f167bf793ba3c11c
unic0rn9k <unic0rn9k@protonmail.com>
2022-02-22 12:20:02 +0100

    Better badge

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-//! # Numbers
-//!
-//! [![Rust](https://github.com/unic0rn9k/num/actions/workflows/rust.yml/badge.svg?label=tests&logo=github)](https://github.com/unic0rn9k/num/actions/workflows/rust.yml)
+//! # Numbers [![GitHub Workflow Status](https://img.shields.io/github/workflow/status/unic0rn9k/num/Rust?label=tests&logo=github)](https://github.com/unic0rn9k/num/actions/workflows/rust.yml)
 //!
 //! # Thanks
 //! Heavy inspiration has been taken from crates listed bellow.
