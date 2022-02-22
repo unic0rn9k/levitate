0dd5b726b0b6324c39accd165879cd318c86b347
unic0rn9k <unic0rn9k@protonmail.com>
2022-02-22 12:09:13 +0100

    Added badge to readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! # Numbers
 //!
+//! [![GitHub Workflow Status](https://img.shields.io/github/workflow/status/unic0rn9k/num/Tests?label=tests&logo=github)](https://github.com/unic0rn9k/num/actions/workflows/rust.yml)
+//!
 //! # Thanks
 //! Heavy inspiration has been taken from crates listed bellow.
 //!
