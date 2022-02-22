5f94db740bbf6d66d74dbbafacef8c8dccfc7f04
unic0rn9k <unic0rn9k@protonmail.com>
2022-02-22 06:32:20 +0100

    Moved `fast_floats` into module inside this repo

---
+++ src/fast_floats.rs
@@ -0,0 +1,247 @@
+//! # Fast Floats
+//! This project is forked from [bluss/fast-floats](https://github.com/bluss/fast-floats).
+//!
+//! **Changes from original project:**
+//! - Fast floats implement deref to their source float type
+//!
+//! # Original docs
+//! [Docs for `Fast` struct ](https://docs.rs/fast-floats/latest/fast_floats/struct.Fast.html)
+//!
+//! Experimental (unstable) “fast-math” wrappers for f32, f64
+//!
+//! These wrappers enable the [“fast-math”][1] flags for the operations
+//! where there are intrinsics for this (add, sub, mul, div, rem).
+//! The wrappers exist so that we have a quick & easy way **to experiment**
+//! with fast math flags and further that feature in Rust.
+//!
+//! Note that as of this writing, the Rust instrinsics use the “fast” flag
+//! documented in the langref; this enables all the float flags.
+//!
+//! [1]: https://llvm.org/docs/LangRef.html#fast-math-flags
+//!
+//! # Rust Version
+//!
+//! This crate is nightly only and experimental. Breaking changes can occur at
+//! any time, if changes in Rust require it.
+
+use core::intrinsics::{fadd_fast, fdiv_fast, fmul_fast, frem_fast, fsub_fast};
+use core::ops::{
+    Add, AddAssign, Deref, DerefMut, Div, DivAssign, Mul, MulAssign, Rem, RemAssign, Sub, SubAssign,
+};
+
+/// “fast-math” wrapper for f32 and f64.
+///
+/// The `Fast` type enforces no invariant and can hold any f32, f64 values.
+/// See crate docs for more details.
+#[derive(Copy, Clone, PartialEq, PartialOrd)]
+#[repr(transparent)]
+pub struct Fast<F>(F);
+
+impl<F> const Deref for Fast<F> {
+    type Target = F;
+
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<F> DerefMut for Fast<F> {
+    #[inline(always)]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+/// This is actually a bad idea, but is required for my use cases.
+/// Creating a Fast float should be unsafe - as fast floats use `core_intrinsics`.
+impl<F> From<F> for Fast<F> {
+    #[inline(always)]
+    fn from(f: F) -> Self {
+        Self(f)
+    }
+}
+
+/// “fast-math” wrapper for `f64`
+pub type FF64 = Fast<f64>;
+/// “fast-math” wrapper for `f32`
+pub type FF32 = Fast<f32>;
+
+impl<F> Fast<F> {
+    /// Create a new fast value
+    ///
+    /// # Safety
+    ///
+    /// The value can be used with the `fast_fadd` etc intrinsics without checks after it has been
+    /// created like this. Refer to Rust and other sources for documentation on which operations
+    /// are valid (might change with time).
+    ///
+    /// Be wary of operations creating invalid values in `Fast` which they could potentially do
+    /// depending on the operation.
+    pub const unsafe fn new(value: F) -> Self {
+        Fast(value)
+    }
+}
+
+macro_rules! impl_op {
+    ($($name:ident, $method:ident, $intrins:ident;)*) => {
+        $(
+        // Fast<F> + F
+        impl $name<f64> for Fast<f64> {
+            type Output = Self;
+            #[inline(always)]
+            fn $method(self, rhs: f64) -> Self::Output {
+                unsafe {
+                    Fast($intrins(self.0, rhs))
+                }
+            }
+        }
+
+        impl $name<f32> for Fast<f32> {
+            type Output = Self;
+            #[inline(always)]
+            fn $method(self, rhs: f32) -> Self::Output {
+                unsafe {
+                    Fast($intrins(self.0, rhs))
+                }
+            }
+        }
+
+        // F + Fast<F>
+        impl $name<Fast<f64>> for f64 {
+            type Output = Fast<f64>;
+            #[inline(always)]
+            fn $method(self, rhs: Fast<f64>) -> Self::Output {
+                Fast(self).$method(rhs.0)
+            }
+        }
+
+        impl $name<Fast<f32>> for f32 {
+            type Output = Fast<f32>;
+            #[inline(always)]
+            fn $method(self, rhs: Fast<f32>) -> Self::Output {
+                Fast(self).$method(rhs.0)
+            }
+        }
+
+        // Fast<F> + Fast<F>
+        impl $name for Fast<f64> {
+            type Output = Self;
+            #[inline(always)]
+            fn $method(self, rhs: Self) -> Self::Output {
+                self.$method(rhs.0)
+            }
+        }
+
+        impl $name for Fast<f32> {
+            type Output = Self;
+            #[inline(always)]
+            fn $method(self, rhs: Self) -> Self::Output {
+                self.$method(rhs.0)
+            }
+        }
+        )*
+
+    }
+}
+
+macro_rules! impl_assignop {
+    ($($name:ident, $method:ident, $optrt:ident, $opmth:ident;)*) => {
+        $(
+        impl<F, Rhs> $name<Rhs> for Fast<F>
+            where Self: $optrt<Rhs, Output=Self> + Copy,
+        {
+            #[inline(always)]
+            fn $method(&mut self, rhs: Rhs) {
+                *self = (*self).$opmth(rhs)
+            }
+        }
+        )*
+
+    }
+}
+
+impl_op! {
+    Add, add, fadd_fast;
+    Sub, sub, fsub_fast;
+    Mul, mul, fmul_fast;
+    Div, div, fdiv_fast;
+    Rem, rem, frem_fast;
+}
+
+impl_assignop! {
+    AddAssign, add_assign, Add, add;
+    SubAssign, sub_assign, Sub, sub;
+    MulAssign, mul_assign, Mul, mul;
+    DivAssign, div_assign, Div, div;
+    RemAssign, rem_assign, Rem, rem;
+}
+
+use core::fmt;
+macro_rules! impl_format {
+    ($($name:ident)+) => {
+        $(
+        impl<F: fmt::$name> fmt::$name for Fast<F> {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                self.0.fmt(f)
+            }
+        }
+        )+
+    }
+}
+
+impl_format!(Debug Display LowerExp UpperExp);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! test_op {
+        ($($op:tt)+) => {
+            $(
+                assert_eq!(Fast(2.) $op Fast(1.), Fast(2. $op 1.));
+            )+
+        }
+    }
+
+    #[test]
+    fn each_op() {
+        test_op!(+ - * / %);
+    }
+
+    macro_rules! assign_op {
+        ($($x:literal $op:tt $y:literal is $z:literal ;)+) => {
+            $(
+                let mut x = Fast($x);
+                x $op Fast($y);
+                assert_eq!(x, Fast($z));
+            )+
+        }
+    }
+
+    #[test]
+    fn assign_ops() {
+        assign_op!(
+            1. += 2. is 3.;
+            1. -= 2. is -1.;
+            2. *= 2. is 4.;
+            2. /= 2. is 1.;
+            5. %= 2. is 1.;
+        );
+    }
+
+    #[test]
+    fn deref() {
+        let a = unsafe { FF32::new(2.) };
+        assert_eq!(a.sin(), 2f32.sin())
+    }
+
+    #[test]
+    fn conversion() {
+        let f = |_: FF32| {};
+        f(0f32.into());
+
+        let f = |_: f32| {};
+        unsafe { f(*FF32::new(0.)) };
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,288 @@
 //! - https://lib.rs/crates/num
 //! - https://lib.rs/crates/fast-floats
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+#![feature(const_trait_impl, core_intrinsics)]
+
+use std::ops::*;
+
+pub trait PrimitiveFloat: Float + BackedByPrimitive<Primitive = Self> {}
+
+pub trait BackedByPrimitive {
+    type Primitive;
+    fn from_primitive(f: Self::Primitive) -> Self;
+    fn into_primitive(self) -> Self::Primitive;
+    fn from_f64(f: f64) -> Self;
+}
+
+pub trait Float:
+    Add<Self, Output = Self>
+    + Div<Self, Output = Self>
+    + Mul<Self, Output = Self>
+    + Sub<Self, Output = Self>
+    + Copy
+    + BackedByPrimitive
+    + PartialEq
+{
+    const _0: Self;
+    const _1: Self;
+    const _2: Self;
+
+    fn sqrt_(self) -> Self;
+    fn powi_(self, p: i32) -> Self;
+    fn hypot_(self, other: Self) -> Self;
+    fn exp_(self) -> Self::Primitive;
+    fn sin_(self) -> Self::Primitive;
+    fn cos_(self) -> Self::Primitive;
+}
+
+macro_rules! impl_wrapper {
+	($fn: ident $( ($($arg: ident $({$($pre: tt)*})? : $t: ty),*) )? { $($finalize: tt)* } -> $re: ty) => {
+		paste!{
+            fn [< $fn _ >] (self $(, $($arg: $t),* )? ) -> $re {
+                self . $fn($($( $arg $($($pre)*)? ),*)?) $($finalize)*
+            }
+        }
+	};
+}
+
+macro_rules! def_primitive {
+    ($t: ty) => {
+        impl const BackedByPrimitive for $t {
+            type Primitive = $t;
+
+            #[inline(always)]
+            fn from_primitive(f: Self::Primitive) -> Self {
+                f
+            }
+
+            #[inline(always)]
+            fn into_primitive(self) -> Self::Primitive {
+                self
+            }
+
+            #[inline(always)]
+            fn from_f64(f: f64) -> Self {
+                f as $t
+            }
+        }
+
+        impl const PrimitiveFloat for $t {}
+
+        impl Float for $t {
+            const _0: $t = 0.;
+            const _1: $t = 1.;
+            const _2: $t = 2.;
+
+            impl_wrapper!(powi (p: i32) { .into() } -> Self);
+            impl_wrapper!(hypot (other: Self) { .into() } -> Self);
+
+            impl_wrapper!(sqrt { .into() } -> Self);
+            impl_wrapper!(exp { .into() } -> Self);
+            impl_wrapper!(sin { .into() } -> Self);
+            impl_wrapper!(cos { .into() } -> Self);
+        }
+    };
+}
+
+def_primitive!(f32);
+def_primitive!(f64);
+
+#[cfg(feature = "fast-floats")]
+pub mod fast_floats;
+#[cfg(feature = "fast-floats")]
+pub mod _fast_floats {
+    use super::*;
+    use fast_floats::*;
+
+    macro_rules! def_fast_primitive {
+    ($t: ty) => {
+        impl const BackedByPrimitive for Fast<$t> {
+            type Primitive = $t;
+
+            #[inline(always)]
+            fn from_primitive(f: Self::Primitive) -> Self {
+                unsafe{ Self::new(f) }
+            }
+
+            #[inline(always)]
+            fn into_primitive(self) -> Self::Primitive {
+                *self
+            }
+
+            #[inline(always)]
+            fn from_f64(f: f64) -> Self {
+                unsafe{ Self::new(f as $t) }
+            }
+        }
+
+        impl Float for Fast<$t> {
+            const _0: Self = unsafe{ Fast::new(0.) };
+            const _1: Self = unsafe{ Fast::new(1.) };
+            const _2: Self = unsafe{ Fast::new(2.) };
+
+            impl_wrapper!(powi (p: i32) { .into() } -> Self);
+            impl_wrapper!(hypot (other { .deref().clone() } : Self) { .into() } -> Self);
+
+            impl_wrapper!(sqrt { .into() } -> Self);
+            impl_wrapper!(exp { .into() } -> $t);
+            impl_wrapper!(sin { .into() } -> $t);
+            impl_wrapper!(cos { .into() } -> $t);
+        }
+    };
+}
+
+    def_fast_primitive!(f32);
+    def_fast_primitive!(f64);
+}
+
+macro_rules! impl_complex_plus_min {
+    ($op: tt, $trait: ident, $fn: ident) => {
+        impl<T: Float> $trait<Self> for Complex<T> {
+            type Output = Self;
+            fn $fn(self, other: Self) -> Self {
+                Self::Output{re: self.re $op other.re, im: self.im $op other.im}
+            }
+        }
+    };
+}
+
+impl_complex_plus_min!(+, Add, add);
+impl_complex_plus_min!(-, Sub, sub);
+
+impl<T: Float> Mul<Self> for Complex<T> {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        let re = self.re * other.re - self.im * other.im;
+        let im = self.re * other.im + self.im * other.re;
+        Self::Output { re, im }
     }
 }
+impl<T: Float> Div<Self> for Complex<T> {
+    type Output = Self;
+    fn div(self, other: Self) -> Self {
+        let re = self.re * other.re + self.im * other.im;
+        let im = self.re * other.im - self.im * other.re;
+        Self::Output { re, im }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[repr(C)]
+pub struct Complex<T: Float> {
+    pub re: T,
+    pub im: T,
+}
+
+impl<T: Float> From<T> for Complex<T> {
+    fn from(n: T) -> Self {
+        Self { re: n, im: T::_0 }
+    }
+}
+
+impl<T: PrimitiveFloat> BackedByPrimitive for Complex<T> {
+    type Primitive = T;
+
+    fn from_primitive(f: Self::Primitive) -> Self {
+        Self { re: f, im: T::_0 }
+    }
+
+    fn into_primitive(self) -> Self::Primitive {
+        //eprint!("Converting complex number to primitive should be done with self.re, instead of self.into_primitive.");
+        self.re.into_primitive()
+    }
+
+    fn from_f64(f: f64) -> Self {
+        Self {
+            re: T::from_f64(f),
+            im: T::_0,
+        }
+    }
+}
+
+impl<T: PrimitiveFloat> Float for Complex<T> {
+    const _0: Self = Self {
+        re: T::_0,
+        im: T::_0,
+    };
+    const _1: Self = Self {
+        re: T::_1,
+        im: T::_0,
+    };
+    const _2: Self = Self {
+        re: T::_2,
+        im: T::_0,
+    };
+
+    /// This function os currently unimplemented for complex numbers, you can still use `number * number`.
+    fn powi_(self, n: i32) -> Self {
+        let mut prod = Self::_1;
+        for _ in 0..n {
+            prod = prod * self
+        }
+        prod
+    }
+
+    fn sqrt_(self) -> Self {
+        let norm = self.re.hypot_(self.im);
+        Self {
+            re: ((norm + self.re) / T::_2).sqrt_(),
+            im: ((norm - self.re) / T::_2).sqrt_(),
+        }
+    }
+
+    fn hypot_(self, other: Self) -> Self {
+        (self * self + other * other).sqrt_()
+    }
+
+    fn exp_(self) -> T {
+        self.re.cos_() + self.im * self.re.sin_()
+    }
+
+    fn cos_(self) -> T {
+        let mut neg_self = self;
+        neg_self.re = self.re * T::from_f64(-1.);
+        (self.exp_() + (neg_self).exp_()) / T::from_f64(2.)
+    }
+
+    //FIXME
+    fn sin_(self) -> T {
+        todo!()
+    }
+}
+
+impl<T: Float> From<[T; 2]> for Complex<T> {
+    fn from(n: [T; 2]) -> Self {
+        Complex { re: n[0], im: n[1] }
+    }
+}
+
+use paste::paste;
+macro_rules! gen_tests {
+    ($t: ty, $name: ident) => {
+        paste! {
+            #[cfg(test)]
+            #[test]
+            fn $name() {
+                for x in 0..100 {
+                    let x = $t::from_f64(x as f64) / 2.;
+                    for y in 0..100 {
+                        let y = $t::from_f64(y as f64) / 2.;
+                        assert_eq!(x.hypot(y.into_primitive()), x.hypot_(y).into_primitive());
+                    }
+                    for n in 0..4 {
+                        assert_eq!(x.powi(n), x.powi_(n).into_primitive());
+                    }
+                    assert_eq!(x.sqrt(), x.sqrt_().into_primitive());
+                }
+            }
+        }
+    };
+}
+
+gen_tests!(f32, trig_f32);
+gen_tests!(f64, trig_f64);
+
+#[cfg(test)]
+use fast_floats::Fast;
+gen_tests!(Fast::<f32>, trig_fast_f32);
+gen_tests!(Fast::<f64>, trig_fast_f64);
